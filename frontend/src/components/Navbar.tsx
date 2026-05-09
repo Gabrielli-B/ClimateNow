3ebc634ed4bbd672b202566a9342519e3b2e884e
Gabrielli <gabrielliborba42@gmail.com>
2026-05-09 16:22:08 -0300

    feat: adiciona estilo ao Navbar e index

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,18 +1,22 @@
+import"./Navbar.css";
+import logo from "../assets/icone_clima_sol_nuvem.png";
 
 export function Navbar(){
     return(
         <header className="navbar"> 
-            <h1 className="logoHome">ClimateNow</h1>
+
+            <div className="title">
+                <img src={logo} alt="Logo ClimateNow" />
+                <h1>Climate</h1>
+                <h1 className="titleColor">Now</h1>
+            </div>
 
             <nav>
                 <a href="#">Sobre</a>
                 <a href="#">Recursos</a>
                 <a href="#">Contato</a>
+                <button className="btn-login">entrar</button>
             </nav>
-
-            <button className="btn-login">
-                entrar
-            </button>
         </header>
     );
 }
