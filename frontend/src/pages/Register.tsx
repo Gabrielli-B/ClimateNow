c2379566f77cab2828dd369b971128c9d2065b05
Gabrielli <gabrielliborba42@gmail.com>
2026-05-12 09:37:32 -0300

    feat: cria estrutura de página de cadastro

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,17 +1,39 @@
+import iconClimateNow from "../assets/icon_climateNow.png";
+
 export function Register() {
   return (
-    <div>
-      <h1>Cadastro</h1>
 
-      <input type="text" placeholder="Nome" />
+    <div className="container-register">
+    
+    <section className="register-card">
 
-      <input type="email" placeholder="Email" />
+        <img src={iconClimateNow} alt="Logo ClimateNow" />
+        <h1>Climate</h1>
+        <h1>Now</h1>
 
-      <input type="password" placeholder="Senha" />
+        <nav>
+            <a href="#">Sobre</a>
+            <a href="#">Recursos</a>
+            <a href="#">Contato</a>
+            <a href="#">Cadastrar</a>
+        </nav>
 
-      <button>
-        Cadastrar
-      </button>
+
+        <form action="">
+            <h1>Bem-vindo!</h1>
+            <p>Crie sua conta para continuar</p>
+
+        <input type="text" placeholder="Nome" />
+
+        <input type="email" placeholder="Email" />
+
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Cadastrar</button>
+
+        </form>
+
+      </section>
     </div>
   );
 }
