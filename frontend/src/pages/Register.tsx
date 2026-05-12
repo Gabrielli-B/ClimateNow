4d45377bcc98b852257ebab12fe3bbd21124fcc5
Gabrielli <gabrielliborba42@gmail.com>
2026-05-12 07:41:53 -0300

    feat: cria tela de cadastro

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,17 @@
+export function Register() {
+  return (
+    <div>
+      <h1>Cadastro</h1>
+
+      <input type="text" placeholder="Nome" />
+
+      <input type="email" placeholder="Email" />
+
+      <input type="password" placeholder="Senha" />
+
+      <button>
+        Cadastrar
+      </button>
+    </div>
+  );
+}
