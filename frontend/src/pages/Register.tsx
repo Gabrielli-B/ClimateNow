6f0cc0ebddf6b3b3a4b75a7629c19c52505e5028
Gabrielli <gabrielliborba42@gmail.com>
2026-05-12 20:35:28 -0300

    refactor: reorganiza header e ajusta estilos da página Register e Navbar

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,6 @@
 import iconClimateNow from "../assets/icon_climateNow.png";
 import "./Register.css";
-
+import { Link } from "react-router-dom";
 
 export function Register() {
   return (
@@ -11,15 +11,19 @@ export function Register() {
       
       <header>
 
-        <img src={iconClimateNow} alt="Logo ClimateNow" />
-        <h1>Climate</h1>
-        <h1 className="titleColor">Now</h1>
-
+        <div className="logo-text">
+          <img src={iconClimateNow} alt="Logo ClimateNow" />
+          <h1>Climate</h1>
+          <h1 className="titleColor">Now</h1>
+        </div>
+        
         <nav>
             <a href="#">Sobre</a>
             <a href="#">Recursos</a>
             <a href="#">Contato</a>
-            <a href="#">Cadastrar</a>
+            <Link to="/login">
+                    <button className="btn-login">Entrar</button>
+            </Link>
         </nav>
 
       </header>
