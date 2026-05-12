df5d8fdd5e94f1545ce0392ed1ee8edf6492fba5
Gabrielli <gabrielliborba42@gmail.com>
2026-05-12 19:23:36 -0300

    refactor: ajusta disposição de elementos na página Register

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,6 @@
 import iconClimateNow from "../assets/icon_climateNow.png";
+import "./Register.css";
+
 
 export function Register() {
   return (
@@ -6,6 +8,8 @@ export function Register() {
     <div className="container-register">
     
     <section className="register-card">
+      
+      <header>
 
         <img src={iconClimateNow} alt="Logo ClimateNow" />
         <h1>Climate</h1>
@@ -18,22 +22,35 @@ export function Register() {
             <a href="#">Cadastrar</a>
         </nav>
 
+      </header>
 
-        <form action="">
+      <div className="content-register">
+        
+        <div className="left-side">
+          <h2>
+            Informação que <br/>
+            transforma o <span>amanhã</span>
+          </h2>
+
+          <p>
+            Acompanhe o clima em tempo real, receba alertas
+              e ajude a construir um futuro mais consciente.
+          </p>
+        </div>
+
+        <div className="right-side">
+          <form>
             <h1>Bem-vindo!</h1>
             <p>Crie sua conta para continuar</p>
 
-        <input type="text" placeholder="Nome" />
-
-        <input type="email" placeholder="Email" />
-
-        <input type="password" placeholder="Senha" />
-
-        <button type="submit">Cadastrar</button>
-
-        </form>
-
-      </section>
-    </div>
+            <input type="text" placeholder="Nome" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Senha" />
+            <button type="submit">Cadastrar</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
   );
 }
