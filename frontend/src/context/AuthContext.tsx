a7adeea0763305569e266e01df4fccebf2e79dee
Gabrielli <gabrielliborba42@gmail.com>
2026-05-15 09:34:11 -0300

    feat: adiciona autenticação mock com login, registro e persistência via localStorage

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,9 +1,17 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState,useEffect } from "react";
 import type { ReactNode } from "react";
+
+type User ={
+    name: string;
+    email: string;
+    password: string;
+};
 
 type AuthContextType = {
   isAuthenticated: boolean;
-  login: () => void;
+  user: User | null;
+  login: (email: string, password: string) => boolean;
+  register: (user: User) => void;
   logout: () => void;
 };
 
@@ -14,21 +22,65 @@ type AuthProviderProps = {
 const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+   const [user, setUser] = useState<User | null>(null);
 
-  function login() {
-    setIsAuthenticated(true);
+  useEffect(() => {
+    const userStorage = localStorage.getItem("@user");
+
+    if (userStorage) {
+      setUser(JSON.parse(userStorage));
+    }
+  }, []);
+
+  function register(userData: User) {
+    localStorage.setItem(
+      "@registeredUser",
+      JSON.stringify(userData)
+    );
+  }
+
+  function login(email: string, password: string) {
+    const registeredUser = localStorage.getItem("@registeredUser");
+
+    if (!registeredUser) {
+      return false;
+    }
+
+    const parsedUser: User = JSON.parse(registeredUser);
+
+    const emailCorrect =
+      parsedUser.email === email;
+
+    const passwordCorrect =
+      parsedUser.password === password;
+
+    if (emailCorrect && passwordCorrect) {
+      localStorage.setItem(
+        "@user",
+        JSON.stringify(parsedUser)
+      );
+
+      setUser(parsedUser);
+
+      return true;
+    }
+
+    return false;
   }
 
   function logout() {
-    setIsAuthenticated(false);
+    localStorage.removeItem("@user");
+
+    setUser(null);
   }
 
   return (
     <AuthContext.Provider
       value={{
-        isAuthenticated,
+        isAuthenticated: !!user,
+        user,
         login,
+        register,
         logout,
       }}
     >
