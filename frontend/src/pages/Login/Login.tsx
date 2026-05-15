a1e4ca6b7229648350307c4a0cee106a0c87ea95
Gabrielli <gabrielliborba42@gmail.com>
2026-05-15 19:02:55 -0300

    feat: adiciona validação de login, cadastro de usuário e navegação autenticada mock

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -2,25 +2,39 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import "./Login.css"
 import { AuthLayout } from "../../layouts/AuthLayout/AuthLayout";
+import { useState } from "react";
 
 export function Login() {
   const { login } = useAuth();
 
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  function handleLogin() {
-    login();
+  function handleLogin(event: React.FormEvent) {
+    event.preventDefault();
 
-    navigate("/dashboard");
+    const success = login(email, password);
+
+    if (success) {
+      navigate("/dashboard");
+
+      return;
+    }
+
+    alert("Email ou senha inválidos");
   }
 
   return (
     <AuthLayout>
-      <form>
+      <form onSubmit={handleLogin}>
         <h1>Bem-vindo de volta!</h1>
         <p>Faça Login para continuar</p>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
+
+        <input type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/>
+
+        <input type="password" placeholder="Senha" onChange={(e)=> setPassword(e.target.value)} />
+        
         <button className="btn-login" onClick={handleLogin}>Entrar</button>
       </form>
     </AuthLayout>
--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,20 +1,47 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { AuthLayout } from "../../layouts/AuthLayout/AuthLayout";
+import { useAuth } from "../../context/AuthContext";
 import "./Register.css"
 
 export function Register() {
+
+  const{ register } = useAuth();
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleRegister(event: React.FormEvent) {
+    event.preventDefault();
+
+    register({
+      name,
+      email,
+      password,
+    });
+
+    alert("Cadastro realizado!");
+    navigate("/login");
+  }
   return (
 
     <AuthLayout>
 
-      <form>
+      <form onSubmit={handleRegister}>
         <h1>Bem-vindo!</h1>
+
         <p>Crie sua conta para continuar</p>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
+
+        <input type="text" placeholder="Nome" onChange={(e)=>setName(e.target.value)}/>
+
+        <input type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
+
+        <input type="password" placeholder="Senha" onChange={(e)=>setPassword(e.target.value)} />
+
         <button className="btn-singUp" type="submit">Cadastrar</button>
 
       </form>
     </AuthLayout>
-  );
+   );
 }
