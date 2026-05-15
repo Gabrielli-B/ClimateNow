dd48cd77419e0e79a812b9ff84ceefb651fabc0f
Gabrielli <gabrielliborba42@gmail.com>
2026-05-15 07:23:27 -0300

    feat:cria estilização de login reutilizando de register

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+import iconClimateNow from "../assets/icon_climateNow.png";
+import "./Login.css"
 
 export function Login() {
   const { login } = useAuth();
@@ -13,16 +16,56 @@ export function Login() {
   }
 
   return (
-    <div>
-      <h1>Login</h1>
+    
+    <div className="container-register">
+    
+    <section className="register-card">
+      
+      <header>
 
-      <input type="email" placeholder="Digite seu email" />
+        <div className="logo-text">
+          <img src={iconClimateNow} alt="Logo ClimateNow" />
+          <h1>Climate</h1>
+          <h1 className="titleColor">Now</h1>
+        </div>
+        
+        <nav>
+            <a href="#">Sobre</a>
+            <a href="#">Recursos</a>
+            <a href="#">Contato</a>
+            <Link to="/register">
+                    <button className="btn-signUp">Cadastrar-se</button>
+            </Link>
+        </nav>
 
-      <input type="password" placeholder="Digite sua senha" />
+      </header>
 
-      <button onClick={handleLogin}>
-        Entrar
-      </button>
-    </div>
+      <div className="content-register">
+        
+        <div className="left-side">
+          <h2>
+            Informação que <br/>
+            transforma o <span>amanhã</span>
+          </h2>
+
+          <p>
+            Acompanhe o clima em tempo real, receba alertas
+              e ajude a construir um futuro mais consciente.
+          </p>
+        </div>
+
+        <div className="right-side">
+          <form>
+            <h1>Bem-vindo de volta!</h1>
+            <p>Faça Login para continuar</p>
+
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Senha" />
+            <button onClick={handleLogin}>Entrar</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
   );
 }
